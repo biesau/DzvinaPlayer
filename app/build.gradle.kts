dd9f8af43541df12dd343c39e09903222fd19f55
Alhierd <alhierd@vkl.by>
2026-04-09 15:56:10 +0300

    Icon fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.maxvale.dzvinaplayer"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
